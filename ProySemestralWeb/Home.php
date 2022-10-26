389cb67109fd1843cb7c3c261164914c3e4d4152
zandukoide <104593879+zandukoide@users.noreply.github.com>
2022-10-26 15:01:17 -0500

    Add files via upload

---
+++ ProySemestralWeb/Home.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fifa World Cup 2022</title>
+    <link rel="stylesheet" href="Home.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="contenedor-menu">
+                <div class="contenedor-imagen-mundial">
+                    <a href=""><img src="qatar2022.png" alt=""></a>
+                </div>
+                <div class="contenedor-botones-principales">
+                    <a href=""`><button class="botones-menu">Equipos</button></a>
+                    <a href=""><button class="botones-menu">Partidos</button></a>
+                    <a href=""><button class="botones-menu">Resultados</button></a>
+                    <a href=""><button class="botones-menu">Clasificación</button></a>
+                    <a href=""><button class="botones-menu">Favoritos</button></a>
+                </div>
+                <div class="contenedor-usuario">
+                    <div class="contenedor-imagen-usuario">
+                        <a href=""><img src="usuario.png" alt=""></a>
+                    </div>
+                    <div class="contenedor-nombre-usuario">
+                        <a href="" class="nombre-usuario"><h3>Usuario</h3></a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
