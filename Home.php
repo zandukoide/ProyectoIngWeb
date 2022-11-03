3a68e9e50ea2c8aafc5028509b7c3e7e13694037
Robertito <robertocortes291402@gmail.com>
2022-11-03 17:51:11 -0500

    Se agregan Inicio de sesion y se modifica Home

---
+++ Home.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="CSS/HomeStyle.css" rel="stylesheet" type="text/css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Fifa World Cup 2022</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="contenedor-menu">
+                <div class="contenedor-imagen-mundial">
+                    <a href=""><img src="IMGs\qatar2022.png" alt=""></a>
+                </div>
+                <ul class="menu">
+                    <li><a href="#inicio" class="seleccion">Inicio</a></li>
+                    <li><a href="#equipos">Equipos</a></li>
+                    <li><a href="#partidos">Partidos</a></li>
+                    <li><a href="#resultados">Resultados</a></li>
+                    <li><a href="#clasificacion">Clasificación</a></li>
+                    <li><a href="#favoritos">Favoritos</a></li>
+                </ul>
+                <div class="contenedor-usuario">
+                    <div class="contenedor-registrarse">
+                        <a href="" class="registrarse">Registrarse</a>
+                    </div>
+                    <hr class="linea-usuario">
+                    <div class="contenedor-iniciarsesion">
+                        <a href="" class="iniciar-sesion">Iniciar sesión</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+</body>
+</html>
