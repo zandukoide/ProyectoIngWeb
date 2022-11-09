3f023ec9d9461e851e4d4892694a6e7d63ad0b0a
Robertito <robertocortes291402@gmail.com>
2022-11-09 17:01:52 -0600

    Se agrega la pagina de equipos

---
+++ Equipos.php
@@ -0,0 +1,278 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="CSS/HomeStyle.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="CSS/EquipoStyle.css" type="text/css">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Fifa World Cup 2022</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <nav>
+            <div class="contenedor-menu">
+                <div class="contenedor-imagen-mundial">
+                    <a href=""><img src="IMGs\qatar2022.png" alt=""></a>
+                </div>
+                <ul class="menu">
+                    <li><a href="C:\xampp\htdocs\ProySemestralWeb\Home.php">Inicio</a></li>
+                    <li><a href="#equipos" class="seleccion">Equipos</a></li>
+                    <li><a href="#partidos">Partidos</a></li>
+                    <li><a href="#resultados">Resultados</a></li>
+                    <li><a href="#clasificacion">Clasificación</a></li>
+                    <li><a href="#favoritos">Favoritos</a></li>
+                </ul>
+                <div class="contenedor-usuario">
+                    <div class="contenedor-registrarse">
+                        <a href="" class="registrarse">Registrarse</a>
+                    </div>
+                    <hr class="linea-usuario">
+                    <div class="contenedor-iniciarsesion">
+                        <a href="" class="iniciar-sesion">Iniciar sesión</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    </header>
+    <span>Grupo A</span>
+    <div class="contenedor-equipo">
+        <a href="#">
+            <img src="IMGs\banderaqatar.webp" alt="">
+            <span>Qatar</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderaecuador.jpg" alt="">
+            <span>Ecuador</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderasenegal.png" alt="">
+            <span>Senegal</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderaholanda.png" alt="">
+            <span>Países Bajos</span>
+        </a>
+    </div>
+    <span>Grupo B</span>
+    <div class="contenedor-equipo">
+    <a href="#">
+            <img src="IMGs\banderainglaterra.webp" alt="">
+            <span>Inglaterra</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderairan.webp" alt="">
+            <span>Irán</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderausa.webp" alt="">
+            <span>Estados Unidos</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderagales.jpg" alt="">
+            <span>Gales</span>
+        </a>
+    </div>
+    <span>Grupo C</span>
+    <div class="contenedor-equipo">
+    <a href="#">
+            <img src="IMGs\banderaargentina.webp" alt="">
+            <span>Argentina</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderaarabia.png" alt="">
+            <span>Arabia Saudita</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderamexico.webp" alt="">
+            <span>México</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderapolonia.jpg" alt="">
+            <span>Polonia</span>
+        </a>
+    </div>
+    <span>Grupo D</span>
+    <div class="contenedor-equipo">
+    <a href="#">
+            <img src="IMGs\banderafrancia.webp" alt="">
+            <span>Francia</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderaaustralia.jpg" alt="">
+            <span>Australia</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderadinamarca.jpg" alt="">
+            <span>Dinamarca</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderatunisia.png" alt="">
+            <span>Túnez</span>
+        </a>
+    </div>
+    <span>Grupo E</span>
+    <div class="contenedor-equipo">
+    <a href="#">
+            <img src="IMGs\banderaespaña.webp" alt="">
+            <span>España</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderacostarica.jpg" alt="">
+            <span>Costa Rica</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderaalemania.webp" alt="">
+            <span>Alemania</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderajapon.jpg" alt="">
+            <span>Japón</span>
+        </a>
+    </div>
+    <span>Grupo F</span>
+    <div class="contenedor-equipo">
+    <a href="#">
+            <img src="IMGs\banderabelgica.jpeg" alt="">
+            <span>Bélgica</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderacanada.jpg" alt="">
+            <span>Canadá</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderamarruecos.png" alt="">
+            <span>Marruecos</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderacroacia.png" alt="">
+            <span>Croacia</span>
+        </a>
+    </div>
+    <span>Grupo G</span>
+    <div class="contenedor-equipo">
+    <a href="#">
+            <img src="IMGs\banderabrasil.webp" alt="">
+            <span>Brasil</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderaserbia.png" alt="">
+            <span>Serbia</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderasuiza.png" alt="">
+            <span>Suiza</span>
+        </a>
+        <a href="#">
+            <img src="IMGs\banderacamerun.jpg" alt="">
+            <span>Camerún</span>
+        </a>
+    </div>
+    <span>Grupo H</span>
+    <div class="contenedor-equipo">
+    <a href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1200px-Flag_of_Portugal.svg.png" alt="">
+            <span>Portugal</span>
+        </a>
+        <a href="#">
+            <img src="https://static3.depositphotos.com/1003857/242/i/450/depositphotos_2428722-stock-photo-national-flag-ghana.jpg" alt="">
+            <span>Ghana</span>
+        </a>
+        <a href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/945px-Flag_of_Uruguay.svg.png" alt="">
+            <span>Uruguay</span>
+        </a>
+        <a href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/200px-Flag_of_South_Korea.svg.png" alt="">
+            <span>Corea del Sur</span>
+        </a>
+    </div>
+    <footer class="footer">
+    <div class="footer__addr">
+      <h1 class="footer__logo">Your Logo</h1>
+
+      <h2>Contact</h2>
+
+      <address>
+        Your address, 123 Avenue, Country<br>
+
+        <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+      </address>
+    </div>
+
+    <ul class="footer__nav">
+      <li class="nav__item">
+        <h2 class="nav__title">Rewards</h2>
+
+        <ul class="nav__ul">
+          <li>
+            <a href="#">Join Now</a>
+          </li>
+
+          <li>
+            <a href="#">Learn More</a>
+          </li>
+
+          <li>
+            <a href="#">Manage Account</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav__item">
+        <h2 class="nav__title">News & Info</h2>
+
+        <ul class="nav__ul">
+          <li>
+            <a href="#">Press Releases</a>
+          </li>
+
+          <li>
+            <a href="#">About Our Products</a>
+          </li>
+
+          <li>
+            <a href="#">Product Support</a>
+          </li>
+
+          <li>
+            <a href="#">Product Manuals</a>
+          </li>
+
+          <li>
+            <a href="#">Product Registration</a>
+          </li>
+
+          <li>
+            <a href="#">Newsletter Sign Up</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav__item">
+        <h2 class="nav__title">Support</h2>
+
+        <ul class="nav__ul">
+          <li>
+            <a href="#">FAQ</a>
+          </li>
+
+          <li>
+            <a href="#">Help Desk</a>
+          </li>
+
+          <li>
+            <a href="#">Forums</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+    <div class="legal">
+      <p>&copy; 2022 Copyright.</p>
+
+    </div>
+    </footer>
+</body>
+</html>
--- Home.php
+++ Home.php
@@ -17,7 +17,7 @@
                 </div>
                 <ul class="menu">
                     <li><a href="#inicio" class="seleccion">Inicio</a></li>
-                    <li><a href="#equipos">Equipos</a></li>
+                    <li><a href="C:\xampp\htdocs\ProySemestralWeb\Equipos.php">Equipos</a></li>
                     <li><a href="#partidos">Partidos</a></li>
                     <li><a href="#resultados">Resultados</a></li>
                     <li><a href="#clasificacion">Clasificación</a></li>
@@ -35,5 +35,100 @@
             </div>
         </nav>
     </header>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <h1>Hola</h1>
+    <footer class="footer">
+    <div class="footer__addr">
+      <h1 class="footer__logo">Your Logo</h1>
+
+      <h2>Contact</h2>
+
+      <address>
+        Your address, 123 Avenue, Country<br>
+
+        <a class="footer__btn" href="mailto:example@gmail.com">Email Us</a>
+      </address>
+    </div>
+
+    <ul class="footer__nav">
+      <li class="nav__item">
+        <h2 class="nav__title">Rewards</h2>
+
+        <ul class="nav__ul">
+          <li>
+            <a href="#">Join Now</a>
+          </li>
+
+          <li>
+            <a href="#">Learn More</a>
+          </li>
+
+          <li>
+            <a href="#">Manage Account</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav__item">
+        <h2 class="nav__title">News & Info</h2>
+
+        <ul class="nav__ul">
+          <li>
+            <a href="#">Press Releases</a>
+          </li>
+
+          <li>
+            <a href="#">About Our Products</a>
+          </li>
+
+          <li>
+            <a href="#">Product Support</a>
+          </li>
+
+          <li>
+            <a href="#">Product Manuals</a>
+          </li>
+
+          <li>
+            <a href="#">Product Registration</a>
+          </li>
+
+          <li>
+            <a href="#">Newsletter Sign Up</a>
+          </li>
+        </ul>
+      </li>
+
+      <li class="nav__item">
+        <h2 class="nav__title">Support</h2>
+
+        <ul class="nav__ul">
+          <li>
+            <a href="#">FAQ</a>
+          </li>
+
+          <li>
+            <a href="#">Help Desk</a>
+          </li>
+
+          <li>
+            <a href="#">Forums</a>
+          </li>
+        </ul>
+      </li>
+    </ul>
+
+    <div class="legal">
+      <p>&copy; 2022 Copyright.</p>
+
+    </div>
+    </footer>
 </body>
 </html>
