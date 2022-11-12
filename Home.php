acfdcac2ec8ac388425c8354185860618617a8cc
Robertito <robertocortes291402@gmail.com>
2022-11-12 17:04:57 -0600

    Se modifica los archivos Home.php y HomeStyle.css

--- Home.php
+++ Home.php
@@ -6,6 +6,9 @@
     <title>Fifa World Cup 2022</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
 </head>
 <body>
@@ -35,15 +38,50 @@
             </div>
         </nav>
     </header>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
-    <h1>Hola</h1>
+    <a href="">
+      <div class="contenedor-noticias-estadios">
+        <div class="noticias-estadios">
+          <p>Explora los magníficos estadios diseñados por varios de los más aclamados arquitectos
+            en todo el mundo, que darán lugar a los partidos de la copa mundial</p>
+          <button>Ver más</button>
+        </div>
+      </div>
+    </a>
+    <div class="contenedor-trending-general">
+      <div class="contenedor-sticky">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Logo_de_la_Copa_Mundial_de_f%C3%BAtbol_2022.svg/640px-Logo_de_la_Copa_Mundial_de_f%C3%BAtbol_2022.svg.png" alt="">
+        <span>Noticias en tendencia de la Copa Mundial 2022</span>
+      </div>
+      <div class="contenedor-tendencias">
+        <a href="">
+          <div class="tendencia-1">
+            <img src="https://img.asmedia.epimg.net/resizer/CMtNlN7EjCRqNDrG1GlNlk-5k9g=/1952x1098/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/7DD5BW6WUNIFDDJKSEBMT5G53Y.jpg" alt="">
+            <div class="contenedor-texto-noticia">
+              <span>Este es el título</span>
+              <p>Estas son las noticias sobre tendencias que se pondrán en la página de Home</p>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div class="tendencia-2">
+            <img src="https://caretas.pe/wp-content/uploads/2022/06/Doha.jpg" alt="">
+            <div class="contenedor-texto-noticia">
+              <span>Este es el título</span>
+              <p>Estas son las noticias sobre tendencias que se pondrán en la página de Home</p>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div class="tendencia-3">
+            <img src="https://www.losandes.com.ar/resizer/3ZG5Y_VpANsf1c0qc7Cz71ttNZY=/1200x630/smart/cloudfront-us-east-1.images.arcpublishing.com/grupoclarin/6TKNGJ4RYZEO5DFNLNFFGPZYEA.jpg" alt="">
+            <div class="contenedor-texto-noticia">
+              <span>Este es el título</span>
+              <p>Estas son las noticias sobre tendencias que se pondrán en la página de Home</p>
+            </div>
+          </div>
+        </a>
+      </div>  
+    </div>
     <footer class="footer">
     <div class="footer__addr">
       <h1 class="footer__logo">Your Logo</h1>
